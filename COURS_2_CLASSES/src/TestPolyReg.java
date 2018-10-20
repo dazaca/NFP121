c1c37166828d4a997056dd35e09ef146b6a9ba5d
dazaca <zafdav@gmail.com>
2018-10-20 10:50:40 +0200

    Modif STATIC pract.

--- COURS_2_CLASSES/src/TestPolyReg.java
+++ COURS_2_CLASSES/src/TestPolyReg.java
@@ -1,6 +1,25 @@
 // Testing Mascage (ocultación) pour la méthode toString: même nom et signature égale -> OVERRIDING
 
+class aux{ 
+	int j =33; 
+	
+	void increment() {
+		j++;
+		System.out.println("j: "+j+"\n");
+	}
+}
 
+class test{
+			
+			static int i;
+			static aux ax = new aux();
+			
+			void increment(){
+				i++;
+				System.out.println("i: "+i+"\n");
+			}
+			
+		}
 public class TestPolyReg {
 
 	public static void main(String[] args) {
@@ -9,6 +28,14 @@ public class TestPolyReg {
 		PolygoneRegulier p2 = new PolygoneRegulier (5,100);
 		PolygoneRegulier p3 = new PolygoneRegulier (4,100);
 		
+		test t1 = new test();
+		test t2 = new test();
+		
+		t1.increment();
+		t2.increment();
+		
+		t1.ax.increment();
+		t2.ax.increment();
 		
 		System.out.println("Poly p1 = " + p1.toString());
 		System.out.println("Poly p2 = " + p2); // appel implicite de toString()
@@ -25,7 +52,7 @@ public class TestPolyReg {
 		System.out.println(" p1 == p3 : " + p1.equals(p3));		
 		System.out.println(" o1 == o3 : " + b1);
 		System.out.println(" p1 == p3 : " + b2);
-				
+					
 	}
 
 }
