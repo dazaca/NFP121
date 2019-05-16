60fc0f1060439b589237843ac5bcdcbb467e12dd
Nene <root@dazaca>
2019-05-16 20:16:49 +0200

    Exercices TD2 (ex. 2)

--- COURS_18_19_2eme/TD2/td2_2.java
+++ COURS_18_19_2eme/TD2/td2_2.java
@@ -1,40 +1,48 @@
 import static java.lang.System.in;
 import static java.lang.System.out;
-
+import java.util.List;
 import java.util.*;
 
-/// Ecrire un programme qui saisit 6 entiers et les stocke dans un 
-/// tableau, puis affiche le
-/// contenu de ce tableau une fois qu’il est rempli.
-///
 
-public class td2_1 {
+// Ecrire un programme qui saisit un entier au clavier et qui recherche si cet entier
+// appartient au tableau. Au cas où la réponse est positive, l’indice de cet entier dans le tableau est
+// affiché. S’il y a plusieurs occurrences, le dernier indice est affiché.
+
+
+public class td2_2 {
 
 	public static void main(String[] args) {
 
         int a;
         int last;
 		int x = 6;
-		int m[] = new int[x];
-		int sum = 0;
+		//int m[] = new int[x];
+		
+		//	for (int i = 0; i < x; i++) {
+		//		System.out.print("Inserte valor de [" + (i+1) + "]: ");
+		//		Scanner clavier = new Scanner(System.in);
+		//		m[i] = clavier.nextInt();
+		//	}
 
-		for (int i = 0; i < x; i++) {
-			System.out.print("Inserte valor de [" + (i+1) + "]: ");
-			Scanner clavier = new Scanner(System.in);
-			m[i] = clavier.nextInt();
-		}
+		Integer[] m = new Integer[]{12,31,31,4,53,31};		
 
 		System.out.println("Array: " + Arrays.toString(m));
         System.out.println("\nIntroduzca el número a encontrar: ");
-        a = clavier.nextInt();
+		Scanner clavier2 = new Scanner (System.in);
+		a = clavier2.nextInt();
         
         // On transforme l'array dans arraylist pour appliquer la méthode
         // "lastIndexOf()".
-        
-        ArrayList m2 = new ArrayList<>(Arrays.asList(m));
-		last = m2.lastIndexOf(a);
-		System.out.println("\nEl índice asociado a la última ocurrencia es [" + last + "]");
+				
+		List<Integer> intList = new ArrayList<Integer>(Arrays.asList(m));
 		
+		// Pour parcourrir la liste
+		//  for (int m2aux : intList) {
+		//  	System.out.println(m2aux);
+		//  }
+		
+		System.out.println("\nEl índice asociado a la última ocurrencia es [" + (intList.lastIndexOf(a) + 1) + "]");
+	
 	}
 	
 
